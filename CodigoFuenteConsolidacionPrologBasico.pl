df975245ff68f01705799f7755a1931a75d24eb2
kenneth-rr <kenneth.rodriguez@utp.edu.co>
2025-03-13 10:11:08 -0500

    Fix CodigoFuenteConsolidacionPrologBasico.pl
    Utilizo listas para los hechos de genero, creo una propia regla de diferente (myDif) para poder usar la aplicacion hecha en java para mostrar un arbol SLD

--- CodigoFuenteConsolidacionPrologBasico.pl
+++ CodigoFuenteConsolidacionPrologBasico.pl
@@ -1,13 +1,7 @@
 % Hechos de genero
-hombre( jose ).
-hombre( tomas ).
-hombre( jaime ).
+hombre( [ jose, tomas, jaime ] ).
 
-mujer( clara ).
-mujer( isabel ).
-mujer( ana ).
-mujer( patricia ).
-
+mujer( [ clara, isabel, ana, patricia ] ).
 
 % Hechos de progenitores
 progenitor( clara, jose ).
@@ -17,29 +11,34 @@ progenitor( jose, ana ).
 progenitor( jose, patricia ).
 progenitor( patricia, jaime ).
 
-%Regla de diferente
+%Regla de diferente(regla dada)
 dif( X, Y ) :- X \= Y.
 
+%Regla de diferente para crear el arbol SLD
+myDif( X, Y, Result ) :- X = Y, Result is 0, !.
+
+myDif( _, _, Result ) :- Result is 1.
+
 %Reglas solicitadas en el ejercicio 1.3
-es_madre( X ) :- mujer( X ), progenitor( X, _ ).
+es_madre( X ) :- mujer( Mujeres ), member( X , Mujeres ), progenitor( X, _ ).
 
-es_padre( X ) :- hombre( X ), progenitor( X, _ ).
+es_padre( X ) :- hombre( Hombres ), member( X, Hombres ), progenitor( X, _ ).
 
-es_hijo( X ) :- hombre( X ), progenitor( _, X ).
+es_hijo( X ) :- hombre( Hombres ), member( X, Hombres ), progenitor( _, X ).
 
 hermana_de( X, Y ) :- 
-    mujer( X ), 
+    mujer( Mujeres ), member( X , Mujeres ),  
     progenitor( Progenitor, X ), 
     progenitor( Progenitor, Y ), 
-    dif( X, Y ).
+    myDif( X, Y, Result ), Result = 1.
 
 abuelo_de( X, Y ) :- 
-    hombre( X ),
+    hombre( Hombres ), member( X, Hombres ),
     progenitor( X, Hijo ),
     progenitor( Hijo, Y ).
 
 abuela_de( X, Y ) :- 
-    mujer( X ),
+    mujer( Mujeres ), member( X , Mujeres ), 
     progenitor( X, Hijo ),
     progenitor( Hijo, Y ).
 
@@ -47,10 +46,10 @@ hermanos( X, Y ) :-
     progenitor( Progenitor1, X ),
     progenitor( Progenitor2, Y ),
     Progenitor1 = Progenitor2,
-    dif( X, Y ).
+    myDif( X, Y, Result ), Result = 1.
 
 tia_de( X, Y ) :-
-    mujer( X ),
+    mujer( Mujeres ), member( X , Mujeres ), 
     hermana_de( X, Progenitor ),
     progenitor( Progenitor, Y ).
     
